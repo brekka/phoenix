dbc5ee7a77f0985848d9e293f7c656c6a8d09683
Andrew Taylor <andrew@brekka.org>
2016-04-12 08:59:05 +0000

    Fix headers

--- core/src/main/java/org/brekka/phoenix/core/services/impl/factory/SigningImpl.java
+++ core/src/main/java/org/brekka/phoenix/core/services/impl/factory/SigningImpl.java
@@ -1,5 +1,17 @@
-/**
- * Copyright (c) 2014 Digital Shadows Ltd.
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.brekka.phoenix.core.services.impl.factory;
 
@@ -9,7 +21,7 @@ import org.brekka.phoenix.core.services.CryptoFactory.Asymmetric.Signing;
 import org.brekka.xml.phoenix.v2.model.SigningType;
 
 /**
- * @author Andrew Taylor (andy@digitalshadows.com)
+ * @author Andrew Taylor (andrew@brekka.org)
  *
  */
 public class SigningImpl implements Signing {
